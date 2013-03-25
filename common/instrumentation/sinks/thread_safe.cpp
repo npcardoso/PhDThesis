e141c571dccb7287df1d318fdf5a710c7dc2c34d
Nuno Cardoso <nunopcardoso@gmail.com>
2013-03-25 18:15:59 +0000

    Added thread safe sink for constructs

--- common/instrumentation/sinks/thread_safe.cpp
+++ common/instrumentation/sinks/thread_safe.cpp
@@ -1,26 +1,33 @@
 #include "instrumentation/sinks/thread_safe.h"
 
+t_thread_safe_construct_sink::t_thread_safe_construct_sink(t_construct_sink::t_ptr sink): sink(sink) {
+
+}
+
+bool t_thread_safe_construct_sink::operator()(const t_transaction_construct::t_ptr & ctr) {
+  return protect(ctr);
+}
+
+bool t_thread_safe_construct_sink::operator()(const t_oracle_construct::t_ptr & ctr) {
+  return protect(ctr);
+}
+
+bool t_thread_safe_construct_sink::operator()(const t_probe_construct::t_ptr & ctr) {
+  return protect(ctr);
+}
+
 t_thread_safe_observation_sink::t_thread_safe_observation_sink(t_observation_sink::t_ptr sink): sink(sink) {
 
 }
 
 bool t_thread_safe_observation_sink::operator()(const t_transaction_observation::t_ptr & obs) {
-  mutex.lock();
-  bool ret = (*sink)(obs);
-  mutex.unlock();
-  return ret;
+  return protect(obs);
 }
 
 bool t_thread_safe_observation_sink::operator()(const t_oracle_observation::t_ptr & obs) {
-  mutex.lock();
-  bool ret = (*sink)(obs);
-  mutex.unlock();
-  return ret;
+  return protect(obs);
 }
 
 bool t_thread_safe_observation_sink::operator()(const t_probe_observation::t_ptr & obs) {
-  mutex.lock();
-  bool ret = (*sink)(obs);
-  mutex.unlock();
-  return ret;
+  return protect(obs);
 }
--- common/instrumentation/sinks/thread_safe.h
+++ common/instrumentation/sinks/thread_safe.h
@@ -1,12 +1,41 @@
 #ifndef __COMMON_INSTRUMENTATION_SINKS_THREAD_SAFE_H__
 #define __COMMON_INSTRUMENTATION_SINKS_THREAD_SAFE_H__
 
+#include "instrumentation/sinks/construct.h"
 #include "instrumentation/sinks/observation.h"
 #include <boost/thread/mutex.hpp>
+
+class t_thread_safe_construct_sink: public t_construct_sink {
+  boost::mutex mutex;
+  t_construct_sink::t_ptr sink;
+  
+  template <class CTR>
+    bool protect(CTR & ctr) {
+      mutex.lock();
+      bool ret = (*sink)(ctr);
+      mutex.unlock();
+      return ret;
+    }
+public:
+  t_thread_safe_construct_sink(t_construct_sink::t_ptr sink);
+  
+  virtual bool operator()(const t_transaction_construct::t_ptr & ctr);
+  virtual bool operator()(const t_oracle_construct::t_ptr & ctr);
+  virtual bool operator()(const t_probe_construct::t_ptr & ctr);
+
+};
 
 class t_thread_safe_observation_sink: public t_observation_sink {
   boost::mutex mutex;
   t_observation_sink::t_ptr sink;
+  
+  template <class OBS>
+    bool protect(OBS & obs) {
+      mutex.lock();
+      bool ret = (*sink)(obs);
+      mutex.unlock();
+      return ret;
+    }
 public:
   t_thread_safe_observation_sink(t_observation_sink::t_ptr sink);
 

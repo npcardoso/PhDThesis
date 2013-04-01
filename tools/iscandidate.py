b7c8567c769d2fbeaff789bd07c838e3eeb94101
Nuno Cardoso <nunopcardoso@gmail.com>
2013-04-01 18:30:30 +0100

    Fixed iscandidate python tool

--- tools/iscandidate.py
+++ tools/iscandidate.py
@@ -10,10 +10,20 @@ if len(sys.argv) != 2:
 
 spectra = Spectra().read(open(sys.argv[1], 'r'))
 
+candidate = []
 try:
     while True:
-        candidate = [int(x) for x in raw_input().split()]
-        print spectra.is_candidate(candidate)
+        candidate += [int(x) for x in raw_input().split()]
+        try:
+            while True:
+                pos = candidate.index(0);
+                print spectra.is_candidate(candidate[:pos])
+                if pos+1 >= len(candidate):
+                    candidate = []
+                else:
+                    candidate = candidate[pos:]
+        except ValueError:
+            pass
         
 except EOFError:
     pass

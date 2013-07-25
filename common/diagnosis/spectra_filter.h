bae7a27922d60c3907770d70029f6aa836b1a2b0
Nuno Cardoso <nunopcardoso@gmail.com>
2013-07-25 18:08:41 +0100

    Fixed spaces

--- common/diagnosis/spectra_filter.h
+++ common/diagnosis/spectra_filter.h
@@ -17,7 +17,7 @@ class t_spectra_filter {
   t_count filtered_transaction_count;
 
 public:
-  
+
   t_spectra_filter();
 
   t_transaction_id next_transaction(t_transaction_id transaction) const;
@@ -25,20 +25,20 @@ public:
 
   t_component_id get_last_component() const;
   t_transaction_id get_last_transaction() const;
-  
+
   t_component_id get_filtered_component_count() const;
   t_transaction_id get_filtered_transaction_count() const;
-  
+
   bool is_component(t_component_id component) const;
   bool is_transaction(t_transaction_id transaction) const;
 
   void filter_all_components_but(const t_candidate & candidate);
   void filter_component(t_component_id component);
   void filter_transaction(t_transaction_id transaction);
-  
+
   void unfilter_component(t_component_id component);
   void unfilter_transaction(t_transaction_id transaction);
-  
+
   void resize_components(t_component_id size);
   void resize_transactions(t_component_id size);
 };
--- common/diagnosis/spectra_iterator.h
+++ common/diagnosis/spectra_iterator.h
@@ -11,21 +11,21 @@ class t_spectra_iterator {
 private:
   t_count max_components;
   t_count max_transactions;
-  
+
   t_transaction_id transaction;
   t_component_id component;
-  
+
   const t_spectra_filter * filter;
-  
+
   t_component_id _next_component(t_component_id component) const;
   t_transaction_id _next_transaction(t_transaction_id transaction) const;
 
 public:
-  t_spectra_iterator(t_count max_components, 
-                     t_count max_transactions, 
+  t_spectra_iterator(t_count max_components,
+                     t_count max_transactions,
                      const t_spectra_filter * filter = NULL);
 
-  
+
   t_component_id next_component(bool wrap=false);
   t_transaction_id next_transaction(bool wrap=false);
 
@@ -38,9 +38,9 @@ public:
   inline t_component_id get_component() const {
     return component;
   }
-  
+
   void set_transaction(t_transaction_id transaction);
-  
+
   void set_component(t_component_id component);
 };
 

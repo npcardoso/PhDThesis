c3ed9b571dfa11c9845a420668a1aa81add3d998
Nuno Cardoso <nunopcardoso@gmail.com>
2013-08-02 16:07:18 +0100

    Added more tests to t_mhs

--- src/diagnosis/testsuite/mhs.cpp
+++ src/diagnosis/testsuite/mhs.cpp
@@ -1,13 +1,48 @@
 #include <boost/test/unit_test.hpp>
-#include "diagnosis/spectra/count_spectra.h"
 #include "diagnosis/algorithms/mhs.h"
 #include "diagnosis/heuristics/sort.h"
 #include "diagnosis/heuristics/similarity.h"
+#include "diagnosis/spectra/count_spectra.h"
+#include "diagnosis/spectra/randomizer/bernoulli.h"
 
 #include <fstream>
 using namespace diagnosis;
+using namespace diagnosis::algorithms;
+using namespace diagnosis::structs;
 
 BOOST_AUTO_TEST_SUITE(MHS2)
+
+BOOST_AUTO_TEST_CASE(stop_flags) {
+    t_count_spectra spectra;
+    t_bernoulli_randomizer randomizer(0.25, 1);
+    t_heuristic heuristic;
+    t_trie D;
+
+
+    randomizer.n_comp = 100;
+    randomizer.n_tran = 100;
+    randomizer.randomize(spectra);
+
+    heuristic.push(new heuristics::t_ochiai());
+    heuristic.push(new heuristics::t_sort());
+
+    t_mhs mhs(heuristic);
+    mhs.max_candidate_size = 2;
+    mhs.calculate(spectra, D);
+
+    // Max Candidate Size
+    t_trie::iterator it = D.begin();
+
+    while (it != D.end())
+        BOOST_CHECK((it++)->size() <= mhs.max_candidate_size);
+
+    // Max Candidate Size
+    mhs = t_mhs(heuristic);
+    mhs.max_candidates = 4000;
+    D.clear();
+    mhs.calculate(spectra, D);
+    BOOST_CHECK(D.size() <= mhs.max_candidates);
+}
 
 BOOST_AUTO_TEST_CASE(mhs) {
     for (t_id i = 0; i < 30; i++) {
@@ -15,18 +50,23 @@ BOOST_AUTO_TEST_CASE(mhs) {
         diagnosis::structs::t_trie D, D_ref;
 
         std::stringstream s;
-        std::ifstream input;
+        std::ifstream f;
 
+        // Reading Input
         s << "io/mhs/in." << i << ".txt";
-        input.open(s.str().c_str());
-        input >> spectra;
+        f.open(s.str().c_str());
+        f >> spectra;
+        BOOST_CHECK(f.good());
 
-        input.close();
+        f.close();
         s.str("");
 
+        // Reading Output
         s << "io/mhs/out." << i << ".txt";
-        input.open(s.str().c_str());
-        input >> D_ref;
+        f.open(s.str().c_str());
+        f >> D_ref;
+        BOOST_CHECK(f.good());
+        f.close();
 
         t_heuristic heuristic;
         heuristic.push(new heuristics::t_ochiai());
@@ -35,8 +75,6 @@ BOOST_AUTO_TEST_CASE(mhs) {
         algorithms::t_mhs mhs(heuristic);
 
         mhs.calculate(spectra, D);
-        std::cout << D.size() << " ";
-        std::cout << D_ref.size() << std::endl;
         BOOST_CHECK(D == D_ref);
     }
 }

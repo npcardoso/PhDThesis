ed6ab7f2e24a88828e04e7d116ef7995aee08a7d
Nuno Cardoso <nunopcardoso@gmail.com>
2013-08-13 11:44:08 +0100

    Added graphviz export to t_topology

--- src/common/diagnosis/structs/topology.cpp
+++ src/common/diagnosis/structs/topology.cpp
@@ -1,4 +1,8 @@
 #include "topology.h"
+
+#include "utils/rgb.h"
+
+#include <boost/foreach.hpp>
 #include <boost/random/bernoulli_distribution.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
 #include <boost/random/uniform_real_distribution.hpp>
@@ -67,24 +71,32 @@ bool t_fault::gen_error (boost::random::mt19937 & gen) const {
     return error(gen);
 }
 
-t_goodness t_fault::get_goodness () const {
-    return goodness;
-}
-
 bool t_fault::can_pass () const {
     return e_min < 0.5 && goodness > 0;
 }
 
-t_component_id t_link::operator () (boost::random::mt19937 & gen) {
+t_link::t_link (t_probability prob, t_component_id branch1, t_component_id branch2) {
+    assert(prob >= 0);
+    assert(prob <= 1);
+    assert(branch1 > 0);
+
+    this->prob = prob;
+    this->branch1 = branch1;
+    this->branch2 = branch2;
+}
+
+t_component_id t_link::operator () (boost::random::mt19937 & gen) const {
     return bernoulli_distribution<> (prob)(gen) ? branch1 : branch2;
 }
 
 t_topology & t_topology::operator () (t_component_id comp, const t_fault & f) {
+    assert(comp != 0);
     t_fault_bind::iterator it = fault_bind.find(comp);
-
 
     if (it != fault_bind.end())
         faults.erase(it->second);
+    else
+        components.insert(comp);
 
 
     fault_bind[comp] = faults.insert(faults.end(), f);
@@ -92,11 +104,19 @@ t_topology & t_topology::operator () (t_component_id comp, const t_fault & f) {
 }
 
 t_topology & t_topology::operator () (t_component_id comp, const t_link & l) {
+    assert(comp != 0);
     t_interface_bind::iterator it = interface_bind.find(comp);
 
-
-    if (it == interface_bind.end())
+    if (it == interface_bind.end()) {
         interface_bind[comp] = interfaces.insert(interfaces.end(), t_interface());
+        components.insert(comp);
+    }
+
+    if (l.get_branch1())
+        components.insert(l.get_branch1());
+
+    if (l.get_branch2())
+        components.insert(l.get_branch2());
 
 
     interface_bind[comp]->push_back(l);
@@ -120,6 +140,83 @@ const t_fault * t_topology::get_fault (t_component_id comp) const {
     if (it == fault_bind.end())
         return NULL;
     else return &(*it->second);
+}
+
+std::ostream & t_topology::graphviz (std::ostream & out) const {
+    out << "digraph g {\n";
+    BOOST_FOREACH(t_component_id c,
+                  components) {
+        graphviz_component(out, c);
+    }
+
+    BOOST_FOREACH(t_component_id c,
+                  components) {
+        graphviz_links(out, c);
+    }
+
+    out << "}\n";
+
+    return out;
+}
+
+std::ostream & t_topology::graphviz_component (std::ostream & out, t_component_id comp) const {
+    const t_fault * f = get_fault(comp);
+    const t_interface * iface = get_interface(comp);
+
+
+    out << "C" << comp << " [\n";
+    out << "rankdir=LR,\n";
+    out << "shape=record,\n";
+    out << "label=\"{<name> C" << comp;
+
+    if (f) {
+        out << "| {<goodness> " << f->get_goodness();
+        out << "| <failure> " << f->get_failure();
+        out << "} | {<emin> " << f->get_emin();
+        out << "| <emax> " << f->get_emax() << "}";
+    }
+
+    if (iface) {
+        t_id i = 1;
+        BOOST_FOREACH(t_link l,
+                      *iface) {
+            out << " | <L" << i++ << "> " <<
+                l.get_prob() << " ? C" <<
+                l.get_branch1() << " : C" <<
+                l.get_branch2();
+        }
+    }
+
+    out << "}";
+
+    out << "\"];\n";
+
+    return out;
+}
+
+std::ostream & t_topology::graphviz_links (std::ostream & out, t_component_id comp) const {
+    const t_interface * iface = get_interface(comp);
+
+
+    if (iface) {
+        t_id i = 1;
+        BOOST_FOREACH(t_link l,
+                      *iface) {
+            if (l.get_branch1()) {
+                out << "C" << comp << ":L" << i << "->C" << l.get_branch1();
+                out << "[color=\" #" << t_rgb(0, 0, 0, l.get_prob()) << "\"];\n";
+            }
+
+            if (l.get_branch2()) {
+                out << "C" << comp << ":L" << i << "->C" << l.get_branch2();
+                out << "[color=\" #" << t_rgb(0, 0, 0, 1 - l.get_prob()) << "\"];\n";
+            }
+
+            i++;
+        }
+    }
+
+    return out;
 }
 }
 }
--- src/common/diagnosis/structs/topology.h
+++ src/common/diagnosis/structs/topology.h
@@ -18,8 +18,12 @@ public:
     bool gen_error (boost::random::mt19937 & gen) const;
     t_error gen_pass_value (boost::random::mt19937 & gen) const;
     t_error gen_error_value (boost::random::mt19937 & gen) const;
-    t_goodness get_goodness () const;
     bool can_pass () const;
+
+    inline t_error get_emin () const {return e_min;}
+    inline t_error get_emax () const {return e_max;}
+    inline t_probability get_failure () const {return failure;}
+    inline t_goodness get_goodness () const {return goodness;}
 
 private:
     t_error e_min, e_max;
@@ -30,7 +34,12 @@ private:
 class t_link {
 public:
     t_link (t_probability prob, t_component_id branch1, t_component_id branch2=0);
-    t_component_id operator () (boost::random::mt19937 & gen);
+
+    t_component_id operator () (boost::random::mt19937 & gen) const;
+
+    inline t_component_id get_branch1 () const {return branch1;}
+    inline t_component_id get_branch2 () const {return branch2;}
+    inline t_probability get_prob () const {return prob;}
 
 private:
     t_probability prob;
@@ -49,14 +58,21 @@ public:
     const t_interface * get_interface (t_component_id comp) const;
     const t_fault * get_fault (t_component_id comp) const;
 
+    std::ostream & graphviz (std::ostream & out) const;
+
 private:
+    std::ostream & graphviz_component (std::ostream & out, t_component_id comp) const;
+    std::ostream & graphviz_links (std::ostream & out, t_component_id comp) const;
+
     typedef std::list<t_interface> t_interfaces;
     typedef std::list<t_fault> t_faults;
+    typedef std::set<t_component_id> t_components;
     typedef std::map<t_component_id, t_faults::iterator> t_fault_bind;
     typedef std::map<t_component_id, t_interfaces::iterator> t_interface_bind;
 
 private:
 
+    t_components components;
     t_interfaces interfaces;
     t_faults faults;
     t_interface_bind interface_bind;

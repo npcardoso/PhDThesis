cc55ab683e08cb45a601c152d0ebe74f1089ff2b
Nuno Cardoso <nunopcardoso@gmail.com>
2013-11-23 19:08:25 +0000

    Bootstrap script working

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ CLANG_SRC=http://llvm.org/releases/3.3/cfe-3.3.src.tar.gz
 RT_SRC=http://llvm.org/releases/3.3/compiler-rt-3.3.src.tar.gz
 BOOST_SRC=http://downloads.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.gz
 OPENMPI_SRC=http://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.5.tar.gz
+MPFR_SRC=http://www.mpfr.org/mpfr-current/mpfr-3.1.2.tar.gz
 
 LLVM_EXTRA_OPTS=--with-python=/usr/bin/python2
 BOOST_EXTRA_OPTS=--with-python=/usr/bin/python2
@@ -11,23 +12,22 @@ BOOST_EXTRA_OPTS=--with-python=/usr/bin/python2
 BUILD_JOBS=16
 
 
-DEPS=("$LLVM_SRC $CLANG_SRC $RT_SRC $BOOST_SRC $SCONS_SRC $OPENMPI_SRC")
-DEPS_BASENAMES=()
+DEPS=("$LLVM_SRC $CLANG_SRC $RT_SRC $BOOST_SRC $SCONS_SRC $OPENMPI_SRC $MPFR_SRC")
 
 echo "Creating .deps dir"
 
 if [[ ! -e deps ]]; then
     mkdir deps
-    cd deps
 fi
+cd deps
+
 PREFIX=$PWD/build
 
 echo "Downloading stuff"
 for d in $DEPS; do
     BASENAME=`basename $d`
-    DIRNAME=`echo $BASENAME | sed s/\.tar\.gz//`
-    DEP_NAME=`echo $DIRNAME | sed s/[-_].*$//`
-    DEPS_BASENAMES=("$DEPS_BASENAMES $BASENAME")
+    DIRNAME=`echo $BASENAME | sed 's/\.tar\.gz//'`
+    DEP_NAME=`echo $DIRNAME | sed 's/\([a-zA-Z_-]*\)[-_].*$/\1/'`
     echo $BASENAME $DIRNAME $DEP_NAME
 
     if [[ ! -e $BASENAME ]]; then
@@ -43,8 +43,8 @@ for d in $DEPS; do
 done
 
 if [[ ! -e llvm/ready ]]; then
-    ln -s cfe llvm/tools
-    ln -s compiler-rt llvm/projects
+    ln -s ../../cfe llvm/tools/clang
+    ln -s ../../compiler-rt llvm/projects/compiler-rt
 
     cd llvm
 
@@ -111,10 +111,30 @@ if [[ ! -e openmpi/ready ]]; then
     echo "Installing OpenMPI"
     make install
 
-    cd ..
     touch ready
     echo "Finished OpenMPI"
     cd ..
 else
     echo "Not building OpenMPI"
+fi
+
+if [[ ! -e mpfr/ready ]]; then
+    cd mpfr
+
+    echo "Configuring MPFR"
+    configure \
+        --prefix=$PREFIX \
+        $MPFR_EXTRA_OPTS
+
+    echo "Building MPFR"
+    make -j $BUILD_JOBS
+
+    echo "Installing MPFR"
+    make install
+
+    touch ready
+    echo "Finished MPFR"
+    cd ..
+else
+    echo "Not building MPFR"
 fi

c5e22deb9c42285630bc2675f48cc38eaaf645d1
Nuno Cardoso <nunopcardoso@gmail.com>
2013-11-28 18:29:13 +0000

    Added names to generators and rankers in benchmark engine

--- src/common/diagnosis/benchmark/benchmark.cpp
+++ src/common/diagnosis/benchmark/benchmark.cpp
@@ -89,13 +89,47 @@ const t_benchmark & t_benchmark::operator () (const t_spectra & spectra,
 }
 
 t_benchmark & t_benchmark::add_generator (t_candidate_generator::t_ptr & generator) {
+    return add_generator(generator,
+                         boost::lexical_cast<std::string> (generators.size() + 1));
+}
+
+t_benchmark & t_benchmark::add_generator (t_candidate_generator::t_ptr & generator,
+                                          const std::string & name) {
     generators.push_back(generator);
+    generator_ids[name] = generators.size();
     return (*this);
 }
 
 t_benchmark & t_benchmark::add_ranker (t_candidate_ranker::t_ptr & ranker) {
+    return add_ranker(ranker,
+                      boost::lexical_cast<std::string> (rankers.size() + 1));
+}
+
+t_benchmark & t_benchmark::add_ranker (t_candidate_ranker::t_ptr & ranker,
+                                       const std::string & name) {
     rankers.push_back(ranker);
+    ranker_ids[name] = rankers.size();
     return (*this);
+}
+
+t_id t_benchmark::get_generator_id (const std::string & name) const {
+    t_id_map::const_iterator pos = generator_ids.find(name);
+
+
+    if (pos == generator_ids.end())
+        return 0;
+
+    return pos->second;
+}
+
+t_id t_benchmark::get_ranker_id (const std::string & name) const {
+    t_id_map::const_iterator pos = ranker_ids.find(name);
+
+
+    if (pos == ranker_ids.end())
+        return 0;
+
+    return pos->second;
 }
 
 t_benchmark & t_benchmark::add_connection (t_id generator_id, t_id ranker_id) {
@@ -103,6 +137,12 @@ t_benchmark & t_benchmark::add_connection (t_id generator_id, t_id ranker_id) {
     assert(ranker_id > 0);
     connections.insert(t_connection(generator_id - 1, ranker_id - 1));
     return (*this);
+}
+
+t_benchmark & t_benchmark::add_connection (const std::string & generator_id,
+                                           const std::string & ranker_id) {
+    return add_connection(get_generator_id(generator_id),
+                          get_ranker_id(ranker_id));
 }
 }
 }
--- src/common/diagnosis/benchmark/benchmark.h
+++ src/common/diagnosis/benchmark/benchmark.h
@@ -30,13 +30,29 @@ public:
                                      const structs::t_candidate & correct,
                                      t_benchmark_hook & hook) const;
     t_benchmark & add_generator (t_candidate_generator::t_ptr & generator);
+    t_benchmark & add_generator (t_candidate_generator::t_ptr & generator,
+                                 const std::string & name);
 
     t_benchmark & add_ranker (t_candidate_ranker::t_ptr & ranker);
+    t_benchmark & add_ranker (t_candidate_ranker::t_ptr & ranker,
+                              const std::string & name);
 
-    t_benchmark & add_connection (t_id generator_id, t_id ranker_id);
+    t_id get_generator_id (const std::string & name) const;
+    t_id get_ranker_id (const std::string & name) const;
+
+    t_benchmark & add_connection (t_id generator_id,
+                                  t_id ranker_id);
+    t_benchmark & add_connection (const std::string & generator_id,
+                                  const std::string & ranker_id);
+
 
 protected:
     std::set<t_connection> connections;
+
+    typedef std::map<std::string, t_id> t_id_map;
+    t_id_map generator_ids;
+    t_id_map ranker_ids;
+
     std::vector<t_candidate_generator::t_ptr> generators;
     std::vector<t_candidate_ranker::t_ptr> rankers;
 };

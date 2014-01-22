766100f0eeb350c3060fe5cbc10d794a047e1e2f
Nuno Cardoso <nunopcardoso@gmail.com>
2014-01-22 12:12:47 +0000

    Added suspicious_component_count to spectra statistics

--- src/common/diagnosis/benchmark/statistics_hook.cpp
+++ src/common/diagnosis/benchmark/statistics_hook.cpp
@@ -16,13 +16,14 @@ void t_statistics_hook::_init (const structs::t_spectra & spectra,
 
     // Print header on first iteration
     if (open_file("spectra.stats.csv", f, true))
-        f << "date, system, iteration, component_count, transaction_count, activation_rate, error_count, error_rate" << std::endl;
+        f << "date, system, iteration, component_count, transaction_count, suspicious_component_count, activation_rate, error_count, error_rate" << std::endl;
 
     f << "\"" << to_simple_string(second_clock::local_time()) << "\", ";
     f << get_system_count() << ", ";
     f << get_iteration_count() << ", ";
     f << spectra.get_component_count() << ", ";
     f << spectra.get_transaction_count() << ", ";
+    f << spectra.get_suspicious_components_count() << ", ";
     f << spectra.get_activation_rate() << ", ";
     f << spectra.get_error_count() << ", ";
     f << spectra.get_error_rate() << std::endl;

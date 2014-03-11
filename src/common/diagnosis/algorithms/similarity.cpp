2f0ac4330356ccea87db011d8596937f5aa924c7
Nuno Cardoso <nunopcardoso@gmail.com>
2014-03-11 13:03:33 +0000

    Fixed serious bug in ochiai

--- src/common/diagnosis/algorithms/similarity.cpp
+++ src/common/diagnosis/algorithms/similarity.cpp
@@ -126,7 +126,7 @@ t_score t_ochiai::similarity_coefficient (const t_count n[2][2]) const {
         return 0;
 
     t_score tmp = ((n[1][1] + n[0][1]) * (n[1][1] + n[1][0]));
-    return n[1][1] / tmp;
+    return n[1][1] / sqrt(tmp);
 }
 
 std::ostream & t_ochiai::print (std::ostream & out) const {

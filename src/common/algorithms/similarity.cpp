ba003bf1f674e45bf3ebb4b9d086e57db621bd3b
Nuno Cardoso <nunopcardoso@gmail.com>
2014-05-23 14:39:07 +0100

    Fixed tarantula coef

--- src/common/algorithms/similarity.cpp
+++ src/common/algorithms/similarity.cpp
@@ -137,7 +137,7 @@ t_score t_tarantula::similarity_coefficient (const t_count n[2][2]) const {
     t_score tmp1 = n[1][1] / (t_score) (n[1][1] + n[0][1]);
     t_score tmp2 = 0;
 
-    if (!n[1][0])
+    if (n[1][0])
         tmp2 = n[1][0] / (t_score) (n[1][0] + n[0][0]);
 
     return tmp1 / (tmp1 + tmp2);

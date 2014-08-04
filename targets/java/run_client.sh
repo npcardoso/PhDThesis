3e78f99d1ca2f2f10389f1fe637c91573003606c
Alexandre Perez <alexandrecperez@gmail.com>
2014-08-04 12:17:27 -0700

    removed running scripts

--- targets/java/run_client.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-java -version
-java -javaagent:target/javassist-1.0-SNAPSHOT-jar-with-dependencies.jar=1234 \
-    -cp target/javassist-1.0-SNAPSHOT.jar \
-    io.crowbar.sandbox.App 2>&1

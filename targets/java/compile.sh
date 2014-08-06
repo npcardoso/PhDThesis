dfbe4c7ca34a3349ee68dec15dad959d800fd4a4
Nuno Cardoso <nunopcardoso@gmail.com>
2014-08-06 15:28:35 +0100

    Added compilation script

--- targets/java/compile.sh
+++ targets/java/compile.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 set -e
 
-mvn -q $* | grep 'ERROR.*:\[' | sed '1!G;h;$!d' | sort |uniq
+mvn -q $* \
+    | grep 'ERROR.*:\[' \
+    | sed -e "s'\[ERROR\] ''" \
+    -e's/://g' \
+    -e's/\[/,/' \
+    -e's/]/,/g' \
+    -e"s'$PWD/src/\([a-zA-Z]*\)/java/io/crowbar'[\1]'" \
+    -e "s' error''" \
+    | sort -t',' -k1,1dr -k2,2nr -k 3,3n \
+    | sed -e 's/,/\t[/' -e 's/,/:/' -e 's/,/]\t/' \
+    | uniq

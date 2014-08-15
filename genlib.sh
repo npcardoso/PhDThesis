9fd069ef088c9c02be70dfe79a2f5e2dc99e449f
Nuno Cardoso <nunopcardoso@gmail.com>
2014-08-15 11:45:57 +0100

    Maven builds everything

--- genlib.sh
+++ genlib.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-
-
-
-RESOURCES_DIR=target/volatile-resources/linux-x86-64
-LIBDIAG_FILE=../diagnostic-native/install/lib/libdiag.so
-
 set -e
-
-cd diagnostic-native
-scons
-
-cd ../instrumentation-java
-
-mvn clean -q
-
-mkdir -vp $RESOURCES_DIR
-cp -v $LIBDIAG_FILE $RESOURCES_DIR
-
-mvn install -q
-
-cp target/*-jar-with-dependencies.jar ../crowbar.jar
+mvn install
+cp -v glue-java/target/*-jar-with-dependencies.jar crowbar.jar

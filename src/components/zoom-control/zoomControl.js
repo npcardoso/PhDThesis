2ac763364d03b3a041bc0771039f8f0b4b0d9fea
JorgeFF <ei10050@fe.up.pt>
2014-08-21 13:38:58 +0100

    Zoom buttons disabled while moving Zoom directions changed Zoom reset

--- src/components/zoom-control/zoomControl.js
+++ src/components/zoom-control/zoomControl.js
@@ -6,50 +6,79 @@ function ZoomController(elementSel,zoomElement,svg,configuration){
                 svg.attr("transform", "translate(" + d3.event.translate + ")"+"scale(" + d3.event.scale + ")");
             }
         });
+
+    var eventsBlocked = false;
+    function eventsUnlock(){
+        eventsBlocked = false;
+    }
     var events = {up: function(){
-                //alert('ok');
-                var curTranslate = zoomListener.translate();
-                curTranslate[1] -= 100;
-                zoomListener.translate(curTranslate);
-                zoomListener.event(zoomElement.transition().duration(configuration.currentConfig.animationTransitionTime));
-            },
-            down: function(){
-                //alert('ok');
+                if(eventsBlocked)
+                    return;
+                alert('ok');
+                eventsBlocked = true;
                 var curTranslate = zoomListener.translate();
                 curTranslate[1] += 100;
                 zoomListener.translate(curTranslate);
-                zoomListener.event(zoomElement.transition().duration(configuration.currentConfig.animationTransitionTime));
+                zoomListener.event(zoomElement.transition().duration(configuration.currentConfig.animationTransitionTime).each("end", eventsUnlock));
+            },
+            down: function(){
+                if(eventsBlocked)
+                    return;
+                eventsBlocked = true;
+                var curTranslate = zoomListener.translate();
+                curTranslate[1] -= 100;
+                zoomListener.translate(curTranslate);
+                zoomListener.event(zoomElement.transition().duration(configuration.currentConfig.animationTransitionTime).each("end", eventsUnlock));
             },
             right: function(){
-                //alert('ok');
-                var curTranslate = zoomListener.translate();
-                curTranslate[0] += 100;
-                zoomListener.translate(curTranslate);
-                zoomListener.event(zoomElement.transition().duration(configuration.currentConfig.animationTransitionTime));
-            },
-            left: function(){
-                //alert('ok');
+                if(eventsBlocked)
+                    return;
+                eventsBlocked = true;
                 var curTranslate = zoomListener.translate();
                 curTranslate[0] -= 100;
                 zoomListener.translate(curTranslate);
-                zoomListener.event(zoomElement.transition().duration(configuration.currentConfig.animationTransitionTime));
+                zoomListener.event(zoomElement.transition().duration(configuration.currentConfig.animationTransitionTime).each("end", eventsUnlock));
+            },
+            left: function(){
+                if(eventsBlocked)
+                    return;
+                eventsBlocked = true;
+                var curTranslate = zoomListener.translate();
+                curTranslate[0] += 100;
+                zoomListener.translate(curTranslate);
+                zoomListener.event(zoomElement.transition().duration(configuration.currentConfig.animationTransitionTime).each("end", eventsUnlock));
             },            
             zoomIn: function(){
+                if(eventsBlocked)
+                    return;
+                eventsBlocked = true;
                 var curScale = zoomListener.scale();
                 ++curScale
                 if(curScale <= 10){
                     zoomListener.scale(curScale);
-                    zoomListener.event(zoomElement.transition().duration(configuration.currentConfig.animationTransitionTime));
+                    zoomListener.event(zoomElement.transition().duration(configuration.currentConfig.animationTransitionTime).each("end", eventsUnlock));
                 }
+                eventsUnlock();
             },
             zoomOut: function(){
+                if(eventsBlocked)
+                    return;
+                eventsBlocked = true;
                 var curScale = zoomListener.scale();
                 --curScale;
                 if(curScale < 1){
                     curScale = 1;
                 }
                 zoomListener.scale(curScale);
-                zoomListener.event(zoomElement.transition().duration(configuration.currentConfig.animationTransitionTime));
+                zoomListener.event(zoomElement.transition().duration(configuration.currentConfig.animationTransitionTime).each("end", eventsUnlock));
+            },
+            zoomReset: function(){
+                if(eventsBlocked)
+                    return;
+                eventsBlocked = true;
+                zoomListener.scale(1);
+                zoomListener.translate([0,0]);
+                zoomListener.event(zoomElement.transition().duration(configuration.currentConfig.animationTransitionTime).each("end", eventsUnlock));
             }
         };
 
@@ -64,6 +93,7 @@ function ZoomController(elementSel,zoomElement,svg,configuration){
         $('#panRight').click(events.right);
         $('#zoomIn').click(events.zoomIn);
         $('#zoomOut').click(events.zoomOut);
+        $('#zoomReset').click(events.zoomReset);
     }
 
     $.ajax({

0bd5df79cc97dbf32f299953842ef774ddcc01eb
Alexandre Perez <alexandrecperez@gmail.com>
2014-08-22 10:42:35 -0700

    install libcrowbar jar in local maven repository

--- genlib.sh
+++ genlib.sh
@@ -3,3 +3,4 @@
 set -e
 mvn install
 cp -v glue-java/target/*-jar-with-dependencies.jar crowbar.jar
+mvn -q install:install-file -Dfile=crowbar.jar -DgroupId=io.crowbar.libcrowbar -DartifactId=libcrowbar-jar -Dversion=1.0.0 -Dpackaging=jar

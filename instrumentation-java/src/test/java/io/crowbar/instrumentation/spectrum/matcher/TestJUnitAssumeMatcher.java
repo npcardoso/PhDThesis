3340ae8ff3f437a9d47b307f36fe775a64d8ca8f
José <jose@computer.org>
2014-08-23 23:08:10 +0100

    add test cases to io.crowbar.instrumentation.spectrum.matcher package

---
+++ instrumentation-java/src/test/java/io/crowbar/instrumentation/spectrum/matcher/TestJUnitAssumeMatcher.java
@@ -0,0 +1,27 @@
+package io.crowbar.instrumentation.spectrum.matcher;
+
+import static org.junit.Assert.*;
+import io.crowbar.diagnostic.spectrum.EditableSpectrum;
+import io.crowbar.diagnostic.spectrum.activity.Hit;
+import io.crowbar.instrumentation.spectrum.TrM;
+
+import org.junit.Test;
+
+public class TestJUnitAssumeMatcher {
+
+    @Test
+    public void testEmptySpectrum() {
+        JUnitAssumeMatcher jm = new JUnitAssumeMatcher();
+        EditableSpectrum<Hit, TrM> es = new EditableSpectrum<Hit, TrM>();
+        assertEquals(0, jm.matchTransactions(es).cardinality());
+        assertEquals(0, jm.matchComponents(es).cardinality());
+    }
+
+    @Test
+    public void testNonEmptySpectrum() {
+        // TODO
+        // create a Spectrum with components and transactions
+        // test matchTransactions() and matchComponents()
+        fail("to implement");
+    }
+}

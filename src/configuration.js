19478257cea1d7ea634e35be1570fbcc6475e92b
JorgeFF <ei10050@fe.up.pt>
2014-08-25 12:33:21 +0100

    Configuration bug when no config was set before

--- src/configuration.js
+++ src/configuration.js
@@ -24,32 +24,29 @@ function Configuration() {
  };
 
  var self = this;
- var localStorageConf = localStorage.getItem(this.CONFIG_STORE);
- if (localStorageConf == null || !(localStorageConf = JSON.parse(localStorageConf))) {
+
+ this.saveConfig = function() {
+  localStorage.setItem(this.CONFIG_STORE, JSON.stringify(self.currentConfig));
+}
+
+this.saveGradiante = function() {
+  self.saveConfig();
+  self.gradiante = new Gradiant(self.currentConfig.normalGradiante);
+}
+
+this.resetConfig = function(){
+  self.currentConfig = jQuery.extend(this.DEFAULT_CONFIG,  {
+    lastViewed: this.DEFAULT_CONFIG.defaultView
+  });
+  self.saveGradiante();
+}
+
+var localStorageConf = localStorage.getItem(this.CONFIG_STORE);
+if (localStorageConf == null || !(localStorageConf = JSON.parse(localStorageConf))) {
   self.resetConfig();
 }
 else {
   this.currentConfig = localStorageConf;
 }
 
-
-    //[rgba(0, 255, 0, 1), rgba(255, 255, 0, 1), rgba(255, 0, 0, 1)]
-    this.gradiante = new Gradiant(this.currentConfig.normalGradiante);
-
-    this.saveConfig = function() {
-      localStorage.setItem(this.CONFIG_STORE, JSON.stringify(self.currentConfig));
-    }
-
-    this.saveGradiante = function() {
-      self.saveConfig();
-      self.gradiante = new Gradiant(self.currentConfig.normalGradiante);
-    }
-
-    this.resetConfig = function(){
-          self.currentConfig = jQuery.extend(this.DEFAULT_CONFIG,  {
-          lastViewed: this.DEFAULT_CONFIG.defaultView
-          });
-          self.saveGradiante();
-    }
-
-  }
+}

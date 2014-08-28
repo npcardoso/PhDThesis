25712578f919990b4572f259aacedff71c12565e
José <jose@computer.org>
2014-08-28 14:31:23 +0100

    change class under test

--- instrumentation-java/src/test/java/io/crowbar/instrumentation/passes/TestWrapperPassTest.java
+++ instrumentation-java/src/test/java/io/crowbar/instrumentation/passes/TestWrapperPassTest.java
@@ -1,9 +1,11 @@
 package io.crowbar.instrumentation.passes;
 
 import static org.junit.Assert.assertEquals;
-import io.crowbar.instrumentation.examples.Dummy;
+import io.crowbar.instrumentation.events.AbstractEventListener;
+import io.crowbar.instrumentation.examples.junit.AssumeTest;
 import io.crowbar.instrumentation.passes.wrappers.JUnit4TestWrapper;
 import io.crowbar.instrumentation.passes.wrappers.TestWrapper;
+import io.crowbar.instrumentation.runtime.Collector;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -16,13 +18,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestWrapperPassTest {
-    private static String testClass = Dummy.class.getCanonicalName();
+    private static String testClass = AssumeTest.class.getCanonicalName();
 
     private ClassPool cp;
     private CtClass cc;
 
     @Before
     public void setUp () throws NotFoundException {
+        Collector.start("", new AbstractEventListener() {});
         this.cp = ClassPool.getDefault();
         this.cc = this.cp.get(testClass);
     }

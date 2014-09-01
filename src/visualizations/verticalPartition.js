d514659379fc9035a3105c1b9849cb4e8e8ecdf1
JorgeFF <ei10050@fe.up.pt>
2014-09-01 20:19:05 +0100

    Drag and Drop / click node bug corrected

--- src/visualizations/verticalPartition.js
+++ src/visualizations/verticalPartition.js
@@ -57,7 +57,7 @@ function VerticalPartition(data, elementSel, configuration, events) {
 
     var isClicking = false;
     this.click = function(node,noStateSAndZoomReset) {
-        if(isClicking)
+        if(d3.event.defaultPrevented || isClicking)
             return false;
         isClicking = true;
         if(noStateSAndZoomReset){

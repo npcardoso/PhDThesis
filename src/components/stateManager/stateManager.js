925c7142bdab20dae1442ec024794820178c4b8d
JorgeFF <ei10050@fe.up.pt>
2014-09-01 19:46:09 +0100

    Forget to commit this new files in two commit before

---
+++ src/components/stateManager/stateManager.js
@@ -0,0 +1,40 @@
+function StateManager(visualization){
+	var self = this;
+	var stack = [];
+
+
+	this.initRender = function(elementSel){
+		$(elementSel).append('<div class="iButtons"><img src="components/stateManager/backButton.png" /></div>');
+		$('.iButtons').click(self.recoverState);
+		self.setButtonVis();
+	}
+
+	this.setButtonVis = function(){
+		if(self.undoPossible()){
+			$('.iButtons').css('display','block');
+		}
+		else {
+			$('.iButtons').css('display','none');
+		}
+
+	}
+
+	this.undoPossible = function(){
+		return stack.length > 0;
+	}
+
+	this.saveState = function (){
+		stack.push([visualization.clicked,visualization.zoomEvents.getZoom()]);
+		self.setButtonVis();
+	}
+
+	this.recoverState = function(){
+		if(!self.undoPossible()){
+			return;
+		}
+		lastState = stack.pop();
+		visualization.click(lastState[0],false);
+		visualization.zoomEvents.setZoom(lastState[1]);
+		self.setButtonVis();
+	}
+}

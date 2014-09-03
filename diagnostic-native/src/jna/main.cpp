df4f4379ad4fd8bf7fecdaca708308f2907d642c
Alexandre Perez <alexandrecperez@gmail.com>
2014-09-02 23:30:50 -0700

    commented out standard output calls in jna wrapper

--- diagnostic-native/src/jna/main.cpp
+++ diagnostic-native/src/jna/main.cpp
@@ -25,7 +25,6 @@ using namespace diagnostic::configuration;
 #define MSG_TYPE_DIAGNOSTIC "diagnostic"
 
 
-
 std::ostream & json_write_diagnostic_response (std::ostream & out,
                                                const t_diagnostic_system & ds,
                                                const t_diagnostic_report & dr) {
@@ -43,10 +42,11 @@ void run_diagnostic (const ptree & pt,
                      char ** response) {
     stringstream ss;
 
-    t_const_ptr<t_diagnostic_system> ds = construct_diagnostic_system (pt.get_child("system"));
+
+    t_const_ptr<t_diagnostic_system> ds = construct_diagnostic_system(pt.get_child("system"));
 
     t_count_spectrum spectrum;
-    ss << pt.get<std::string>("spectrum");
+    ss << pt.get<std::string> ("spectrum");
     ss >> spectrum;
 
     t_basic_runner runner;
@@ -59,30 +59,28 @@ void run_diagnostic (const ptree & pt,
     *response = strdup(ss.str().c_str());
 }
 
-
 void run (const char * request,
           char ** response) {
-
     ptree pt;
-    stringstream ss (request);
+    stringstream ss(request);
 
-    read_json (ss, pt);
 
-    string request_type = pt.get<string>("type", string());
+    read_json(ss, pt);
 
-    if(request_type == MSG_TYPE_DIAGNOSTIC)
+    string request_type = pt.get<string> ("type", string());
+
+    if (request_type == MSG_TYPE_DIAGNOSTIC)
         run_diagnostic(pt, response);
 
 
-
-    std::cout << " -----Request----- " << std::endl;
-    std::cout << request << std::endl;
-    std::cout << " -----Request----- " << std::endl;
-    std::cout << " -----Response----- " << std::endl;
-    std::cout << *response << std::endl;
-    std::cout << " -----Response----- " << std::endl;
+    // std::cout << " -----Request----- " << std::endl;
+    // std::cout << request << std::endl;
+    // std::cout << " -----Request----- " << std::endl;
+    // std::cout << " -----Response----- " << std::endl;
+    // std::cout << *response << std::endl;
+    // std::cout << " -----Response----- " << std::endl;
 }
 
-void cleanup(char * response) {
+void cleanup (char * response) {
     free(response);
 }

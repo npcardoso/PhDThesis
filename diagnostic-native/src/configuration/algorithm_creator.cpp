5cfb4d9f4a5e0954750b9f70c2fdd69c8ba5e919
Alexandre Perez <alexandrecperez@gmail.com>
2014-09-09 15:16:01 -0700

    removed static initialization from algorithm_creators

--- diagnostic-native/src/configuration/algorithm_creator.cpp
+++ diagnostic-native/src/configuration/algorithm_creator.cpp
@@ -1,41 +1,49 @@
 #include "algorithm_creator.h"
 
+#include "mhs_creator.h"
+#include "fuzzinel_creator.h"
+#include "similarity_creator.h"
+#include "single_fault_creator.h"
 #include "algorithm_creator_combiner.h"
 
 
 namespace diagnostic {
 namespace configuration {
+// static t_algorithm_creator_combiner DEFAULT;
 
-static t_algorithm_creator_combiner DEFAULT;
+// t_algorithm_creator::t_register::t_register(const t_const_ptr<t_algorithm_creator> ac) {
+// DEFAULT.add(ac);
+// }
 
-t_algorithm_creator::t_register::t_register(const t_const_ptr<t_algorithm_creator> ac) {
-    DEFAULT.add(ac);
+t_ptr<t_algorithm_creator> t_algorithm_creator::get_default () {
+    t_algorithm_creator_combiner * tmp = new t_algorithm_creator_combiner();
+
+
+    tmp->add(t_const_ptr<t_algorithm_creator> (new t_single_fault_creator()));
+    tmp->add(t_const_ptr<t_algorithm_creator> (new t_mhs_creator()));
+    tmp->add(t_const_ptr<t_algorithm_creator> (new t_fuzzinel_creator()));
+    tmp->add(t_const_ptr<t_algorithm_creator> (new t_similarity_creator()));
+    return t_ptr<t_algorithm_creator> (tmp);
 }
 
-
-const t_algorithm_creator & t_algorithm_creator::get_default() {
-    return DEFAULT;
+t_const_ptr<t_candidate_generator> t_algorithm_creator::generator (const t_algorithm & a) const {
+    return t_const_ptr<t_candidate_generator> ();
 }
 
-
-t_const_ptr<t_candidate_generator> t_algorithm_creator::generator(const t_algorithm & a) const {
-    return t_const_ptr<t_candidate_generator>();
+t_const_ptr<t_candidate_ranker> t_algorithm_creator::ranker (const t_algorithm & a) const {
+    return t_const_ptr<t_candidate_ranker> ();
 }
 
-t_const_ptr<t_candidate_ranker> t_algorithm_creator::ranker(const t_algorithm & a) const {
-    return t_const_ptr<t_candidate_ranker>();
-}
-
-const std::string * t_algorithm_creator::get(const t_algorithm & a,
-                                       const std::string & key) const {
+const std::string * t_algorithm_creator::get (const t_algorithm & a,
+                                              const std::string & key) const {
     const t_algorithm::t_configs & configs = a.get_configs();
     t_algorithm::t_configs::const_iterator it = configs.find(key);
 
-    if(it == configs.end())
+
+    if (it == configs.end())
         return NULL;
 
     return &(it->second);
 }
-
 }
 }
--- diagnostic-native/src/configuration/fuzzinel_creator.cpp
+++ diagnostic-native/src/configuration/fuzzinel_creator.cpp
@@ -10,25 +10,25 @@ using namespace std;
 
 namespace diagnostic {
 namespace configuration {
-
-static t_algorithm_creator::t_register registration(t_const_ptr<t_algorithm_creator>(new t_fuzzinel_creator()));
+// static t_algorithm_creator::t_register registration(t_const_ptr<t_algorithm_creator>(new t_fuzzinel_creator()));
 
 t_const_ptr<t_candidate_ranker> t_fuzzinel_creator::ranker (const t_algorithm & a) const {
     t_const_ptr<t_candidate_ranker> ret;
 
-    if(a.get_name() != "fuzzinel")
+    if (a.get_name() != "fuzzinel")
         return ret;
 
     t_fuzzinel * fuzzinel = new t_fuzzinel();
 
     const string * str;
-    if((str = get(a, "use_fuzzy_error")) != NULL)
+
+    if ((str = get(a, "use_fuzzy_error")) != NULL)
         fuzzinel->use_fuzzy_error = lexical_cast<bool> (*str);
 
-    if((str = get(a, "use_confidence")) != NULL)
-        fuzzinel->use_confidence= lexical_cast<bool> (*str);
+    if ((str = get(a, "use_confidence")) != NULL)
+        fuzzinel->use_confidence = lexical_cast<bool> (*str);
 
-    ret = t_ptr<t_candidate_ranker>(fuzzinel);
+    ret = t_ptr<t_candidate_ranker> (fuzzinel);
 
     return ret;
 }

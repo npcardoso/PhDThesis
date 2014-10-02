48886be391d2b4a474fec820d5ade70f26e93910
Nuno Cardoso <nunopcardoso@gmail.com>
2014-10-02 13:25:59 +0100

    Removed unused scripts

--- genlib.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-mvn install
-cp -v instrumentation-java/target/*-jar-with-dependencies.jar crowbar.jar
-mvn -q install:install-file -Dfile=crowbar.jar -DgroupId=io.crowbar.libcrowbar -DartifactId=libcrowbar-jar -Dversion=1.0.0-SNAPSHOT -Dpackaging=jar
--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -cp glue-java/target/*-jar-with-dependencies.jar $*

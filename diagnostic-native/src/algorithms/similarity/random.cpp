db646c597b2bae09c6d9cd564de5c58322592e41
Nuno Cardoso <nunopcardoso@gmail.com>
2015-03-26 15:01:56 +0000

    Fixed locale related bug

--- diagnostic-native/src/algorithms/similarity/random.cpp
+++ diagnostic-native/src/algorithms/similarity/random.cpp
@@ -1,6 +1,6 @@
 #include "random.h"
 
-#include <boost/random/uniform_real_distribution.hpp>
+#include <random>
 
 namespace diagnostic {
 namespace algorithms {
@@ -8,7 +8,7 @@ t_score t_random::operator () (const t_spectrum & spectrum,
                                t_component_id comp,
                                const t_spectrum_filter * filter) const {
     std::mt19937 gen;
-    boost::random::uniform_real_distribution<t_error> rand(0, 1);
+    std::uniform_real_distribution<t_error> rand(0, 1);
 
 
     return rand(gen);
